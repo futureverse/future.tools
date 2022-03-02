13bab98331e6d3a7186f46d1b0679235ccb37948
Henrik Bengtsson <hb@aroma-project.org>
2022-03-02 10:19:13 -0800

    Add with_journals()

---
+++ R/with_journals.R
@@ -0,0 +1,27 @@
+#' Evaluate an R expression while collecting journals from completed futures
+#' 
+#' @param expr The R expression to evaluate
+#'
+#' @param substitute If TRUE, then `expr` is subtituted, otherwise not.
+#'
+#' @param envir The environment where `expr` should be evaluated
+#'
+#' @return
+#' A list of FutureJournal:s. 
+#'
+#' @example incl/ggjournal,future.apply.R
+#'
+#' @export
+with_journals <- function(expr, substitute = TRUE, envir = parent.frame()) {
+  oopts <- options(future.journal = TRUE)
+  on.exit(options(oopts))
+  
+  journals <- NULL
+  withCallingHandlers({
+    eval(expr, envir = envir)
+  }, FutureJournalCondition = function(cond) {
+    journals <<- c(journals, list(cond$journal))
+  })
+  
+  journals
+}
--- incl/ggjournal.R
+++ incl/ggjournal.R
@@ -1,6 +1,4 @@
 library(future)
-oopts <- options(future.journal = TRUE)
-journal <- future:::journal
 
 slow_fcn <- function(x) {
   Sys.sleep(x / 10)
@@ -8,24 +6,26 @@ slow_fcn <- function(x) {
 }
 
 plan(sequential)
-fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
-vs <- value(fs)
-js <- journal(fs)
+js <- with_journals({
+  fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
+  vs <- value(fs)
+})
 print(ggjournal(js))
 
 plan(multisession, workers = 2)
-fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
-vs <- value(fs)
-js <- journal(fs)
+js <- with_journals({
+  fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
+  vs <- value(fs)
+})
 print(ggjournal(js))
 
 ## Decrease polling time to 0.01 seconds (default is 0.1 seconds)
 options(future.wait.interval = 0.01)
-fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
-vs <- value(fs)
-js <- journal(fs)
+js <- with_journals({
+  fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
+  vs <- value(fs)
+})
 print(ggjournal(js))
 
 ## Shut down parallel workers
 plan(sequential)
-options(oopts)
