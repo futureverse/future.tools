1a156410fb3333c70328465853a3c80f2286d0a9
Henrik Bengtsson <hb@aroma-project.org>
2022-06-03 17:38:56 -0700

    HELP: Add Details section to gather_journals()

--- R/gather_journals.R
+++ R/gather_journals.R
@@ -6,8 +6,15 @@
 #'
 #' @param envir The environment where `expr` should be evaluated
 #'
+#' @details
+#' This function evaluates an R expression and gathers the journals
+#' signaled by futures as they are completed. A future journal comprise
+#' a log of events appearing during the life-span of a future, e.g.
+#' the timestamps when the future was created, launched, queried,
+#' resolved, and its results are collected.
+#'
 #' @return
-#' A list of FutureJournal:s. 
+#' A list of [future::FutureJournal]:s.
 #'
 #' @example incl/gather_journals.R
 #'
