d99dc6ec821b997aa2f073faa93b18d74fd824fc
Henrik Bengtsson <hb@aroma-project.org>
2022-06-10 19:37:41 -0700

    Fix ylim too

--- incl/ggjournal,future.apply.R
+++ incl/ggjournal,future.apply.R
@@ -10,13 +10,13 @@ plan(sequential)
 js <- capture_journals({
   vs <- future_lapply(5:1, FUN = slow_fcn)
 })
-print(ggjournal(js) + xlim(0.0, 2.0))
+print(ggjournal(js) + xlim(0.0, 2.0) + ylim(5, 0))
 
 plan(multisession, workers = 2)
 js <- capture_journals({
   vs <- future_lapply(5:1, FUN = slow_fcn)
 })
-print(ggjournal(js) + xlim(0.0, 2.0))
+print(ggjournal(js) + xlim(0.0, 2.0) + ylim(5, 0))
 
 ## Shut down parallel workers
 plan(sequential)
