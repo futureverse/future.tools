d7e7113e3fcdaf740bc9b590c7a7846e1882a319
Henrik Bengtsson <hb@aroma-project.org>
2022-06-10 19:19:06 -0700

    Use same x-lim for all ggjournal example plots

--- incl/ggjournal,future.apply.R
+++ incl/ggjournal,future.apply.R
@@ -1,4 +1,5 @@
 library(future.apply)
+library(ggplot2)
 
 slow_fcn <- function(x) {
   Sys.sleep(x / 10)
@@ -9,13 +10,13 @@ plan(sequential)
 js <- capture_journals({
   vs <- future_lapply(5:1, FUN = slow_fcn)
 })
-print(ggjournal(js))
+print(ggjournal(js) + xlim(0.0, 2.0))
 
 plan(multisession, workers = 2)
 js <- capture_journals({
   vs <- future_lapply(5:1, FUN = slow_fcn)
 })
-print(ggjournal(js))
+print(ggjournal(js) + xlim(0.0, 2.0))
 
 ## Shut down parallel workers
 plan(sequential)
--- incl/ggjournal.R
+++ incl/ggjournal.R
@@ -1,4 +1,5 @@
 library(future)
+library(ggplot2)
 
 slow_fcn <- function(x) {
   Sys.sleep(x / 10)
@@ -10,14 +11,14 @@ js <- capture_journals({
   fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
   vs <- value(fs)
 })
-print(ggjournal(js))
+print(ggjournal(js) + xlim(0.0, 2.0))
 
 plan(multisession, workers = 2)
 js <- capture_journals({
   fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
   vs <- value(fs)
 })
-print(ggjournal(js))
+print(ggjournal(js) + xlim(0.0, 2.0))
 
 ## Increase polling time to 0.1 seconds (default is 0.01 seconds)
 options(future.wait.interval = 0.1)
@@ -25,7 +26,7 @@ js <- capture_journals({
   fs <- lapply(5:1, FUN = function(x) future(slow_fcn(x)))
   vs <- value(fs)
 })
-print(ggjournal(js))
+print(ggjournal(js) + xlim(0.0, 2.0))
 
 ## Shut down parallel workers
 plan(sequential)
