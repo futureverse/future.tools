f6c8a4c81a97bc72bb025a90d20810feb73aa195
Henrik Bengtsson <hb@aroma-project.org>
2022-06-21 10:45:25 -0700

    BETA: export journal() from this package for now - later from 'future'

---
+++ R/100.journal.R
@@ -0,0 +1,61 @@
+#' Gets the logged journal of events for a future
+#'
+#' _WARNING: This function is under development. It can change at any time.
+#' For now, please, do not depend on this function in a published R package._
+#'
+#' @param x A [Future] object.
+#'
+## @param baseline (POSIXct; optional) A timestamp to server as time zero
+## for the relative start time (`at`). If `TRUE` (default), then the
+## earliest timepoint observed is used as the baseline.
+#'
+#' @param \ldots Not used.
+#'
+#' @return
+#' A data frame of class `FutureJournal` with columns:
+#'
+#'  1. `event` (character string)
+#'  2. `type` (character string)
+#'  3. `parent` (character string)
+#'  4. `start` (POSIXct)
+#'  5. `at` (difftime)
+#'  6. `duration` (difftime)
+#'  7. `future_label` (character string)
+#'  8. `future_uuid` (character string)
+#'  9. `session_uuid` (character string)
+#'
+#' Common events are:
+#'
+#'  * `create`   - the future was created (an `overhead`)
+#'  * `launch`   - the future was launched (an `overhead`)
+#'  * `evaluate` - the future was evaluated (an `evaluation`)
+#'  * `resolved` - the future was queried (may be occur multiple times) (an `overhead`)
+#'  * `gather`   - the results was retrieved (an `overhead`)
+#'
+#' but others may be added by other Future classes.
+#'
+#' Common event types are:
+#'
+#'  * `evaluation` - processing time is spent on evaluation
+#'  * `overhead`   - processing time is spent on orchestrating the future
+#'  * `waiting`    - processing time is spent on waiting to set up or querying the future
+#'
+#' but others may be added by other Future classes.
+#'
+#' The data frame is sorted by the `at` time.
+#' Note that the timestamps for the `evaluate` event are based on the local
+#' time on the worker. The system clocks on the worker and the calling R
+#' system may be out of sync.
+#'
+#' @section Enabling and disabling event logging:
+#' To enable logging of events, set option `future.journal` is TRUE.
+#' To disable, set it to FALSE (default).
+#'
+#' @example incl/journal.R
+#'
+#' @seealso
+#' Use [capture_journals()] to capture journals from all futures.
+#'
+#' @keywords internal
+#' @export
+journal <- import_future("journal")
