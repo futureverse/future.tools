571d427ae1c9715b9662112c8d14ea4ec2bb9648
Henrik Bengtsson <hb@aroma-project.org>
2022-06-21 10:35:34 -0700

    BETA: export capture_journals() in this package for now

---
+++ R/100.import_from_future.R
@@ -0,0 +1,22 @@
+#' Evaluate an R expression while collecting journals from completed futures
+#' 
+#' @param expr The R expression to evaluate
+#'
+#' @param substitute If TRUE, then `expr` is subtituted, otherwise not.
+#'
+#' @param envir The environment where `expr` should be evaluated
+#'
+#' @details
+#' This function evaluates an R expression and capture the journals
+#' signaled by futures as they are completed. A future [journal] comprise
+#' a log of events appearing during the life-span of a future, e.g.
+#' the timestamps when the future was created, launched, queried,
+#' resolved, and its results are collected.
+#'
+#' @return
+#' A list of \link[future:journal]{FutureJournal}:s.
+#'
+#' @example incl/capture_journals.R
+#'
+#' @export
+capture_journals <- import_future("capture_journals")
